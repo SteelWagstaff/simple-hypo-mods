25c36a0af7850c2b384f65a8a81d46b8f04af8c1
Steel Wagstaff <swagstaff@wisc.edu>
2018-06-20 16:00:33 -0500

    Improved resizer
    Testing for more efficient resizing when hypothesis is activated

--- js/resize.js
+++ js/resize.js
@@ -1,7 +1,10 @@
 window.hypothesisConfig = function () {
     return {
         onLayoutChange: function(layoutParams) {
-            jQuery('body').css({'padding-right':layoutParams.width});
+          if (body.width() - layoutParams.width() > 400) {
+            jQuery('#page').css({width: 'calc(100%-' + layoutParams.width + 'px)'});
+            // Still need to account for placement of PB nav buttons
         }
-    };
-};
+    }
+	}
+}
