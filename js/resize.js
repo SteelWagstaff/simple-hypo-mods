4c88b6bb4898884d6d2e5461c5bc1aad3b864e3c
Steel Wagstaff <swagstaff@wisc.edu>
2018-06-20 16:33:31 -0500

    Update resize.js

--- js/resize.js
+++ js/resize.js
@@ -1,7 +1,7 @@
 window.hypothesisConfig = function () {
     return {
         onLayoutChange: function(layoutParams) {
-          if (document.body.clientWidth() - layoutParams.width() > 400) {
+          if (document.body.clientWidth - layoutParams.width > 400) {
             jQuery('#page').css({width: 'calc(100%-' + layoutParams.width + 'px)'});
             // Still need to account for placement of PB nav buttons
         }
